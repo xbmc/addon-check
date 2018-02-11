8f8a18babf57d4c7d93b48d633dd4257de29f3a7
mzfr <mehtab.zafar98@gmail.com>
2018-02-11 20:15:13 +0530

    Auto comments issues found on pull requests

--- check_repo.py
+++ check_repo.py
@@ -3,7 +3,7 @@ import sys
 import json
 import check_addon
 from common import colorPrint
-
+from git_comments import GithubAPI
 
 def _read_config_for_version(repo_path):
     config_path = os.path.join(repo_path, '.tests-config.json')
@@ -19,7 +19,6 @@ def check_repo():
     repo_path = os.path.abspath(os.path.join(
         os.path.dirname(os.path.realpath(__file__)), os.pardir))
     print("Repo path " + repo_path)
-
     parameters = sys.argv[1:]
     if len(parameters) == 0:
         toplevel_folders = sorted(next(os.walk(repo_path))[1])
@@ -35,6 +34,14 @@ def check_repo():
             addon_path = os.path.join(repo_path, addon_folder)
             error_counter = check_addon.start(error_counter, addon_path, config)
 
+    if check_addon.check_config(config, "comment_on_pull"):
+        if check_addon.comments:
+             GithubAPI().comment_on_pull(check_addon.comments)
+             GithubAPI().set_label(["Checks failed"])
+        else:
+            GithubAPI().remove_label(["Checks failed"])
+            GithubAPI().set_label(["Checks passed"])
+
     if error_counter["problems"] > 0:
         colorPrint("We found %s problems and %s warnings, please check the logfile." % (
             error_counter["problems"], error_counter["warnings"]), "31")
@@ -43,6 +50,7 @@ def check_repo():
         # If we only found warnings, don't mark the build as broken
         colorPrint("We found %s problems and %s warnings, please check the logfile." % (
             error_counter["problems"], error_counter["warnings"]), "35")
+
 
     print("Finished!")
 
