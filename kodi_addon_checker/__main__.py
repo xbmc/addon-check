065e4e7d8eb503648b94a82892276d168a863d4c
mzfr <mehtab.zafar98@gmail.com>
2018-04-11 23:05:09 +0530

    Add arguments and keep addon.xml caching

--- kodi_addon_checker/__main__.py
+++ kodi_addon_checker/__main__.py
@@ -8,6 +8,8 @@ from kodi_addon_checker.record import Record, PROBLEM, WARNING, INFORMATION
 from kodi_addon_checker.report import Report
 from kodi_addon_checker.reporter import ReportManager
 
+ROOT_URL = "http://mirrors.kodi.tv/addons/{branch}/addons.xml"
+
 
 def dir_type(dir_path):
     """ArgParse callable to validate positional add-on arguments
@@ -27,20 +29,23 @@ def dir_type(dir_path):
     return os.path.abspath(dir_path)
 
 
-def check_artifact(artifact_path, args):
+def check_artifact(artifact_path, args, branch_name):
     """
     Check given artifact and return its report. The artifact can be either an add-on or a repository.
     :param artifact_path: the path of add-on or repo
     :param args: argparse object
     :return: report
     """
+    branch_url = ROOT_URL.format(branch=branch_name)
+    repo_addons = check_addon._get_addons(branch_url)
+
     artifact_path = os.path.abspath(artifact_path)
     config = Config(artifact_path, args)
     ConfigManager.process_config(config)
     if os.path.isfile(os.path.join(artifact_path, "addon.xml")):
-        return check_addon.start(artifact_path, config)
+        return check_addon.start(artifact_path, repo_addons, config)
     else:
-        return check_repo(artifact_path, config)
+        return check_repo(artifact_path, config, repo_addons)
 
 
 def main():
@@ -65,7 +70,7 @@ def main():
         # Following report is a wrapper for all sub reports
         report = Report("")
         for directory in args.dir:
-            report.add(check_artifact(directory, args))
+            report.add(check_artifact(directory, args, args.branch))
     else:
         report = check_artifact(os.getcwd(), args, args.branch)
 
--- kodi_addon_checker/check_repo.py
+++ kodi_addon_checker/check_repo.py
@@ -3,16 +3,11 @@ import kodi_addon_checker.check_addon as check_addon
 from kodi_addon_checker.record import INFORMATION, Record
 from kodi_addon_checker.report import Report
 
-ROOT_URL = "http://mirrors.kodi.tv/addons/{branch}/addons.xml"
 
-
-def check_repo(config, repo_path, parameters, branch_name):
+def check_repo(repo_path, config, repo_addons):
     repo_report = Report(repo_path)
     repo_report.add(Record(INFORMATION, "Checking repository %s" % repo_path))
     toplevel_folders = sorted(next(os.walk(repo_path))[1])
-
-    branch_url = ROOT_URL.format(branch=branch_name)
-    repo_addons = check_addon._get_addons(branch_url)
 
     for addon_folder in toplevel_folders:
         if addon_folder[0] != '.':
