28aef52b7bb11ba2a49d72ef5300afc98af9561c
mzfr <mehtab.zafar98@gmail.com>
2018-06-13 08:00:00 +0200

    refactoring: move files check into separate file

--- kodi_addon_checker/check_addon.py
+++ kodi_addon_checker/check_addon.py
@@ -1,6 +1,4 @@
-import json
 import os
-import re
 import xml.etree.ElementTree as ET
 import requests
 import logging
@@ -16,6 +14,7 @@ from kodi_addon_checker import check_old_addon
 from kodi_addon_checker import check_dependencies
 from kodi_addon_checker import check_entrypoint
 from kodi_addon_checker import handle_files
+from kodi_addon_checker import check_files
 
 REL_PATH = ""
 ROOT_URL = "http://mirrors.kodi.tv/addons/{branch}/addons.xml.gz"
@@ -36,7 +35,7 @@ def start(addon_path, branch_name, all_repo_addons, pr, config=None):
     # Extract common path from addon paths
     # All paths will be printed relative to this path
     REL_PATH = os.path.split(addon_path[:-1])[0]
-    addon_xml = _check_addon_xml(addon_report, addon_path, parsed_xml)
+    addon_xml = check_files.check_addon_xml(addon_report, addon_path, parsed_xml)
 
     if addon_xml is not None:
         if len(addon_xml.findall("*//broken")) == 0:
@@ -44,11 +43,11 @@ def start(addon_path, branch_name, all_repo_addons, pr, config=None):
 
             check_dependencies.check_addon_dependencies(addon_report, repo_addons, parsed_xml)
 
-            _check_for_invalid_xml_files(addon_report, file_index)
+            check_files.check_for_invalid_xml_files(addon_report, file_index)
 
             check_old_addon.check_for_existing_addon(addon_report, addon_path, all_repo_addons, pr)
 
-            _check_for_invalid_json_files(addon_report, file_index)
+            check_files.check_for_invalid_json_files(addon_report, file_index)
 
             check_artwork.check_artwork(addon_report, addon_path, parsed_xml, file_index)
 
@@ -66,7 +65,7 @@ def start(addon_path, branch_name, all_repo_addons, pr, config=None):
                 _check_for_legacy_strings_xml(addon_report, addon_path)
 
             if config.is_enabled("check_legacy_language_path"):
-                _check_for_legacy_language_path(addon_report, addon_path)
+                check_files.check_for_legacy_language_path(addon_report, addon_path)
 
             # Kodi 18 Leia + deprecations
             if config.is_enabled("check_kodi_leia_deprecations"):
@@ -81,59 +80,12 @@ def start(addon_path, branch_name, all_repo_addons, pr, config=None):
             # General blacklist
             _find_blacklisted_strings(addon_report, addon_path, [], [], [])
 
-            _check_file_whitelist(addon_report, file_index, addon_path)
+            check_files.check_file_whitelist(addon_report, file_index, addon_path)
         else:
             addon_report.add(
                 Record(INFORMATION, "Addon marked as broken - skipping"))
 
     return addon_report
-
-
-def _check_for_invalid_xml_files(report: Report, file_index):
-    for file in file_index:
-        if ".xml" in file["name"]:
-            xml_path = os.path.join(file["path"], file["name"])
-            try:
-                # Just try if we can successfully parse it
-                ET.parse(xml_path)
-            except ET.ParseError:
-                report.add(Record(PROBLEM, "Invalid xml found. %s" %
-                                  relative_path(xml_path)))
-
-
-def _check_for_invalid_json_files(report: Report, file_index):
-    for file in file_index:
-        if ".json" in file["name"]:
-            path = os.path.join(file["path"], file["name"])
-            try:
-                # Just try if we can successfully parse it
-                with open(path) as json_data:
-                    json.load(json_data)
-            except ValueError:
-                report.add(Record(PROBLEM, "Invalid json found. %s" %
-                                  relative_path(path)))
-
-
-def _check_addon_xml(report: Report, addon_path, parsed_xml):
-    addon_xml_path = os.path.join(addon_path, "addon.xml")
-    try:
-        handle_files.addon_file_exists(report, addon_path, r"addon\.xml")
-
-        report.add(Record(INFORMATION, "Created by %s" %
-                          parsed_xml.attrib.get("provider-name")))
-        _addon_xml_matches_folder(report, addon_path, parsed_xml)
-    except ET.ParseError:
-        report.add(Record(PROBLEM, "Addon xml not valid, check xml. %s" %
-                          relative_path(addon_xml_path)))
-
-    return parsed_xml
-
-
-def _addon_xml_matches_folder(report: Report, addon_path, addon_xml):
-    if os.path.basename(os.path.normpath(addon_path)) == addon_xml.attrib.get("id"):
-        report.add(Record(INFORMATION, "Addon id matches folder name"))
-    else:
-        report.add(Record(PROBLEM, "Addon id and folder name does not match."))
 
 
 def _check_for_legacy_strings_xml(report: Report, addon_path):
@@ -150,41 +102,6 @@ def _find_blacklisted_strings(report: Report, addon_path, problem_list, warning_
     for result in handle_files.find_in_file(addon_path, warning_list, whitelisted_file_types):
         report.add(Record(WARNING, "Found blacklisted term %s in file %s:%s (%s)"
                           % (result["term"], result["searchfile"], result["linenumber"], result["line"])))
-
-
-def _check_for_legacy_language_path(report: Report, addon_path):
-    language_path = os.path.join(addon_path, "resources", "language")
-    if os.path.exists(language_path):
-        dirs = next(os.walk(language_path))[1]
-        found_warning = False
-        for dir in dirs:
-            if not found_warning and "resource.language." not in dir:
-                report.add(Record(
-                    WARNING, "Using the old language directory structure, please move to the new one."))
-                found_warning = True
-
-
-def _check_file_whitelist(report: Report, file_index, addon_path):
-    if ".module." in addon_path:
-        report.add(Record(INFORMATION, "Module skipping whitelist"))
-        return
-
-    whitelist = (
-        r"\.?(py|xml|gif|png|jpg|jpeg|md|txt|po|json|gitignore|markdown|yml|"
-        r"rst|ini|flv|wav|mp4|html|css|lst|pkla|g|template|in|cfg|xsd|directory|"
-        r"help|list|mpeg|pls|info|ttf|xsp|theme|yaml|dict|crt)?$"
-    )
-
-    for file in file_index:
-        file_parts = file["name"].rsplit(".")
-        # Only check file endings if there are file endings...
-        # This will not check "README" or ".gitignore"
-        if len(file_parts) > 1:
-            file_ending = "." + file_parts[len(file_parts) - 1]
-            if re.match(whitelist, file_ending, re.IGNORECASE) is None:
-                report.add(Record(WARNING,
-                                  "Found non whitelisted file ending in filename %s" %
-                                  relative_path(os.path.join(file["path"], file["name"]))))
 
 
 def _get_addons(xml_url):
