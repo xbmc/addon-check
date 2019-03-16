75f88115503809a9b5a1745d0941eb07db773978
Rechi <Rechi@users.noreply.github.com>
2019-03-16 10:19:45 +0100

    pylint: fix logging-not-lazy

--- kodi_addon_checker/check_addon.py
+++ kodi_addon_checker/check_addon.py
@@ -33,7 +33,7 @@ def start(addon_path, args, all_repo_addons, config=None):
     """
     addon_id = os.path.basename(os.path.normpath(addon_path))
     addon_report = Report(addon_id)
-    LOGGER.info("Checking add-on %s" % addon_id)
+    LOGGER.info("Checking add-on %s", addon_id)
     addon_report.add(Record(INFORMATION, "Checking add-on %s" % addon_id))
 
     repo_addons = all_repo_addons[args.branch]
